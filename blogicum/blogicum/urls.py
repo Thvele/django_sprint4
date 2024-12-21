45e751a2dc52b75cbf3694588d8bab92e0d06ace
Thvele <thvele@mail.ru>
2024-12-21 21:21:50 +0300

    finally

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
 
     path('auth/registration/', CreateView.as_view(
-        template_name='registration/registration.html',
+        template_name='registration/registration_form.html',
         form_class=UserCreationForm,
         success_url=reverse_lazy('login'),
     ), name='registration'),
